9fee736b7bf90f41d354bce5c27fe9a317e6b454
TrieuPN <bettertogetheronit@gmail.com>
2023-08-18 19:32:29 +0700

    Update .bat file

---
+++ pull.bat
@@ -0,0 +1,3 @@
+echo "********** Perform pulling from server **********"
+git pull origin main
+echo "********** Pull Completed **********"
---
+++ push.bat
@@ -0,0 +1,5 @@
+echo "********** Perform pushing code into github **********"
+git add .
+git commit -m "Updated"
+git push origin main
+echo "********** Push completed **********"
